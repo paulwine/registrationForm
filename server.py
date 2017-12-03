951f0dda75e661114936c6d16e7cdee468f8ee35
Paul <paulwinegard@gmail.com>
2017-12-03 01:58:05 -0800

    Main functionality operational.  Redirects correctly inputted form to newuser HTML.  Displays error for incorrect form.

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from flask import Flask, redirect, session, render_template, request, flash
+import re
 
 app = Flask(__name__)
 
 app.secret_key = "SECRETSHIT"
 
+EMAIL_REGEX = re.compile(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$")
+NAME_REGEX = re.compile(r"^[a-zA-z]")
 
 
 @app.route("/")
@@ -13,11 +16,62 @@ def index():
 
 @app.route("/", methods =["POST"])
 def submitForm():
+    if 'error' in session:
+        session.pop("error")
     
-    session["firstName"] = request.form["firstname"]
-    print(session["firstName"])
     
-    return redirect("/newuser")
+    session["first"] = request.form["firstname"]
+    print(session["first"])
+
+    session["last"] = request.form["lastname"]
+    print(session["last"])
+
+    session["email"] = request.form["email"]
+    print(session["email"])
+
+    session["password"] = request.form["password"]
+    print session["password"]
+
+    session["confirm"] = request.form["confirm"]
+    print session["confirm"]
+
+    if len(session["first"]) < 1 or len(session["last"]) < 1 or len(session["email"]) < 1 or len(session["password"]) < 1 or len(session["confirm"]) < 1:
+        flash("INPUT ALL FIELDS DUMMY")
+        
+    if not NAME_REGEX.match(session["first"]):
+        flash("NAMES CANNOT CONTAIN NUMBERS OR SYMBOLS")
+        session["error"] = 1
+        
+    if not NAME_REGEX.match(session["first"]):
+        flash("NAMES CANNOT CONTAIN NUMBERS OR SYMBOLS")
+        session["error"] = 1
+        
+        
+    if not EMAIL_REGEX.match(session["email"]):
+        flash("EMAIL NOT FORMATTED CORRECTLY")
+        session["error"] = 1
+
+    if len(session["password"]) < 8:
+        flash("PASSWORDS MUST BE LONGER THAN 8 CHARACTERS")
+        session["error"] = 1
+
+    if session["password"] != session["confirm"]:
+        flash("PASSWORDS DID NOT MATCH")
+        session["error"] = 1
+
+    # if session[""]
+        
+        session["error"] = 1
+
+    
+    
+    if 'error' in session:
+        return redirect("/")
+    else:
+        return redirect("/newuser")
+
+    
+    
 
 @app.route("/newuser")
 def submitted():
